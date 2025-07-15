b7a995305da14d4b2963ac686e5770ce5b103d2e
Vinicius Sacramento <viniciusjose57@gmail.com>
2025-07-15 11:37:55 +0100

    adding original source code with new AntArena folder to organize projects

---
+++ AntArena/AntArena.cs
@@ -0,0 +1,67 @@
+﻿using Ant_3_Arena.Ants;
+using System;
+using System.Windows.Forms;
+
+namespace Ant_3_Arena
+{
+	public partial class AntArena : Form
+	{
+		private AntRed RedAnt;
+		private AntYellow YellowAnt;
+		private AntBlack BlackAnt;
+		private AntRed RedAnt2;
+		private AntYellow YellowAnt2;
+		private AntBlack BlackAnt2;
+		private AntRed RedAnt3;
+		private AntYellow YellowAnt3;
+		private AntBlack BlackAnt3;
+		public AntArena()
+		{
+			InitializeComponent();
+			this.WindowState = FormWindowState.Maximized;
+			this.BackgroundImage = Properties.Resources.bg;
+
+			RedAnt = new AntRed(this.ClientSize);
+			RedAnt2 = new AntRed(this.ClientSize);
+			RedAnt3 = new AntRed(this.ClientSize);
+			YellowAnt = new AntYellow(this.ClientSize);
+			YellowAnt2 = new AntYellow(this.ClientSize);
+			YellowAnt3 = new AntYellow(this.ClientSize);
+			BlackAnt = new AntBlack(this.ClientSize);
+			BlackAnt2 = new AntBlack(this.ClientSize);
+			BlackAnt3 = new AntBlack(this.ClientSize);
+		}
+
+		private void AntArena_Paint(object sender, PaintEventArgs e)
+		{
+			e.Graphics.DrawImage(RedAnt.AntImage, RedAnt.X, RedAnt.Y, 32, 36);
+			e.Graphics.DrawImage(YellowAnt.AntImage, YellowAnt.X, YellowAnt.Y, 32, 36);
+			e.Graphics.DrawImage(BlackAnt.AntImage, BlackAnt.X, BlackAnt.Y, 32, 36);
+			e.Graphics.DrawImage(RedAnt2.AntImage, RedAnt2.X, RedAnt2.Y, 32, 36);
+			e.Graphics.DrawImage(YellowAnt2.AntImage, YellowAnt2.X, YellowAnt2.Y, 32, 36);
+			e.Graphics.DrawImage(BlackAnt2.AntImage, BlackAnt2.X, BlackAnt2.Y, 32, 36);
+			e.Graphics.DrawImage(RedAnt3.AntImage, RedAnt3.X, RedAnt3.Y, 32, 36);
+			e.Graphics.DrawImage(YellowAnt3.AntImage, YellowAnt3.X, YellowAnt3.Y, 32, 36);
+			e.Graphics.DrawImage(BlackAnt3.AntImage, BlackAnt3.X, BlackAnt3.Y, 32, 36);
+		}
+
+		private void timer1_Tick(object sender, EventArgs e)
+		{
+			RedAnt.Move(this.ClientSize);
+			YellowAnt.Move(this.ClientSize);
+			BlackAnt.Move(this.ClientSize);
+			RedAnt2.Move(this.ClientSize);
+			YellowAnt2.Move(this.ClientSize);
+			BlackAnt2.Move(this.ClientSize);
+			RedAnt3.Move(this.ClientSize);
+			YellowAnt3.Move(this.ClientSize);
+			BlackAnt3.Move(this.ClientSize);
+			Invalidate();
+		}
+
+		private void AntArena_Load(object sender, EventArgs e)
+		{
+			this.DoubleBuffered = true;
+		}
+	}
+}
---
+++ AntArena/Ants/AntYellow.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Drawing;
+
+namespace Ant_3_Arena.Ants
+{
+	public class AntYellow
+	{
+		public int X { get; set; }
+		public int Y { get; set; }
+		private string Direction { get; set; }
+		private int Verticalvelocity = 4;
+		private int Horizontalvelocity = 4;
+		private readonly string color = "#FFFF00";
+		private readonly Bitmap antImage;
+
+		public Bitmap AntImage { get { return antImage; } }
+
+		public AntYellow(Size borders)
+		{
+			Direction = "RightDown";
+			Color newColor = ColorTranslator.FromHtml(color);
+			Color white = ColorTranslator.FromHtml("#FFFFFF");
+
+			Bitmap bmp = new Bitmap(Properties.Resources.Ant);
+
+			for (int x = 0; x < bmp.Width; x++)
+			{
+				for (int y = 0; y < bmp.Height; y++)
+				{
+					Color gotColor = bmp.GetPixel(x, y);
+					if (gotColor == white)
+						bmp.SetPixel(x, y, newColor);
+				}
+			}
+
+			this.antImage = bmp;
+
+			Random random = new Random();
+			X = random.Next(0, borders.Width);
+			Y = random.Next(0, borders.Height);
+		}
+
+		public void Move(Size borders){
+			switch (Direction)
+			{
+				case "LeftUp":
+					X = X - Horizontalvelocity;
+					Y = Y - Verticalvelocity;
+
+					if (X < 0 && Y < 0)
+						Direction = "RightDown";
+					else if (X < 0)
+						Direction = "RightUp";
+					else if (Y < 0)
+						Direction = "LeftDown";
+					break;
+				case "LeftDown":
+					X = X - Horizontalvelocity;
+					Y = Y + Verticalvelocity;
+
+					if (X < 0 && Y > borders.Height)
+						Direction = "RightUp";
+					else if (X < 0)
+						Direction = "RightDown";
+					else if (Y > borders.Height)
+						Direction = "LeftUp";
+					break;
+				case "RightUp":
+					X = X + Horizontalvelocity;
+					Y = Y - Verticalvelocity;
+
+					if (X > borders.Width && Y < 0)
+						Direction = "LeftDown";
+					else if (X > borders.Width)
+						Direction = "LeftUp";
+					else if (Y < 0)
+						Direction = "RightDown";
+					break;
+				case "RightDown":
+					X = X + Horizontalvelocity;
+					Y = Y + Verticalvelocity;
+
+					if (X > borders.Width && Y > borders.Height)
+						Direction = "LeftUp";
+					else if (X > borders.Width)
+						Direction = "LeftDown";
+					else if (Y > borders.Height)
+						Direction = "RightUp";
+					break;
+			}
+		}
+	}
+}
