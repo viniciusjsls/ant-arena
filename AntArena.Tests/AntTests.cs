40afe189697b75f63c34b469e7a0bab8b67ca004
Vinicius Sacramento <viniciusjose57@gmail.com>
2025-07-15 13:36:43 +0100

    add ant first tests before refactoring

---
+++ AntArena.Tests/AntTests.cs
@@ -0,0 +1,24 @@
+﻿using Ant_3_Arena.Ants;
+using Ant_3_Arena.Constants;
+using FluentAssertions;
+using System.Drawing;
+
+namespace AntArena.Tests
+{
+    public class AntTests
+    {
+        [Fact]
+        public void Ant_WhenCreating_ShouldMatchColor()
+        {
+            // Arrange / Act
+            var yellowAnt = new AntYellow(new());
+            var redAnt = new AntRed(new());
+            var blackAnt = new AntBlack(new());
+
+            // Assert
+            yellowAnt.AntImage.GetPixel(yellowAnt.AntImage.Width / 2, yellowAnt.AntImage.Height / 2).Should().Be(ColorTranslator.FromHtml(ColorConstant.YELLOW));
+            redAnt.AntImage.GetPixel(redAnt.AntImage.Width / 2, redAnt.AntImage.Height / 2).Should().Be(ColorTranslator.FromHtml(ColorConstant.RED));
+            blackAnt.AntImage.GetPixel(yellowAnt.AntImage.Width / 2, blackAnt.AntImage.Height / 2).Should().Be(ColorTranslator.FromHtml(ColorConstant.BLACK));
+        }
+    }
+}
